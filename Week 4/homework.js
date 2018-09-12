be0dda19c5cc6125ab2f6ab37992e35a4d77fba4
Josie <jaa287@miami.edu>
2018-09-12 12:09:36 -0400

    loop for repeating

--- Week 4/homework.js
+++ Week 4/homework.js
@@ -1,11 +1,30 @@
+var x = 240;
+var y = 150;
+var d = 30;
+
+
 
 function setup() {
   createCanvas(500,500);
   background(204);
+  noFill();
 }
 
-
 function draw() {
+  // commands that run as many times as defined.
+  //var counter = start_point ; counter < stop_point ; increment in counter
+// for(var i = 1; i < 21; i=i+2){
+//   ellipse(x, y, i*d, i*d);
+// }
+
+for(var i = 1; i < 5; i++)
+  rect(x+i*30, y, d, d)
+
+for(var i = 1; i < 25; i++)
+  line(100+i*10, 250, 100+i*10, 300)
+
+
+
 
 
 }
