90e47e730df1eab0371015f7bbd242ea921887d6
Josie <jaa287@miami.edu>
2018-09-20 13:39:13 -0400

    changing color of line depending on what part of the screen mouse is in

--- Week 5/notes.js
+++ Week 5/notes.js
@@ -1,7 +1,6 @@
 function setup(){
-  createCanvas(1000, 1000);
-  fill(250, 200, 230);
-  stroke(0, 102);
+  createCanvas(300, 300);
+  background(204);
 }
 
 function draw() {
@@ -22,17 +21,37 @@ function draw() {
   //     ellipse(10, 10, 10, 10)
   //   }
 
-var side = true;
 
-if(side) {
-  fill(250, 200, 230);
+// if (mouseY > 0); {
+//   fill(255, 0, 0);
+// }
+// if (mouseX > 100) {
+//   fill(0, 0, 255);
+// }
+// if (mouseX > 200) {
+//   fill(0, 255, 0)
+// }
+// ellipse(mouseX, mouseY, 10, 10);
+
+
+// if(mouseX < 150) {
+//  fill(255, 0, 0); //red
+// } else {
+//  fill(0, 0, 255); //blue
+// }
+//
+// ellipse(mouseX, mouseY, 10, 10);
+// }
+
+if(mouseY > 200) {
+fill(0, 255, 0); //green
 } else {
-  fill(0, 200, 0)
+ if(mouseY > 100) {
+ fill(0, 0, 255); //blue
+ } else {
+ fill(255, 0, 0); //red
+ }
 }
-  ellipse(mouseX, mouseY, 10, 10);
-
-
 
 ellipse(mouseX, mouseY, 10, 10);
-
 }
