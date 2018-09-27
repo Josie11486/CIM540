42358e778c503aea6c0401c3437a8584a8de761b
Josie <jaa287@miami.edu>
2018-09-27 18:25:49 -0400

    kKLmkl;

--- Week 6 Transformations/notes.js
+++ Week 6 Transformations/notes.js
@@ -1,11 +1,27 @@
+angle=0;
 function setup() {
-  createCanvas(480, 480);
+  createCanvas(200, 200);
   background(204);
-  angleMode(DEGREES);
 }
 
 function draw() {
-  rect(200, 0, 40, 40);
-  rotate(90);
-  rect(200, 0, 40, 40);
+
+// push();
+// translate(mouseX, mouseY);
+// fill(255);
+// ellipse(0, 0, 40, 40);
+// // pop();
+// fill(255, 0, 0);
+// rect(100, 100, 40, 40);
+
+
+  // translate(mouseX, mouseY)
+  // rotate(angle);
+  // //what do the first two numbers of the rect do?
+  // rect(-30, -30, 60, 60);
+  // angle += 10;
+
+  // scale(3);
+  // rect(20, 20, 40, 40);
+
 }
