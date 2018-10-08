486f868308377f5400b3ddcd662bdcc86be4371e
Josie <jaa287@miami.edu>
2018-10-08 13:38:38 -0400

    big fish and hallowen r complete

---
+++ Midterm/project.js
@@ -0,0 +1,67 @@
+var halloween;
+var ghosty;
+var skull;
+var x = 0;
+var y = 0;
+var z = 5;
+var easing = 0.02;
+var angle = 0;
+var faster;
+var slower;
+var s = 290;
+
+function preload() {
+  halloween = loadImage('halloween.jpg');
+  ghosty = loadImage('ghosty.png');
+  skull = loadImage('skull.png');
+}
+
+function setup() {
+  createCanvas(800, 480);
+
+  faster = createButton('>');
+  faster.position(360, 370);
+  faster.mousePressed(increaseCount);
+
+  slower = createButton('<');
+  slower.position(315, 370);
+  slower.mousePressed(decreaseCount);
+
+  //when you click trick the skull will spin faster
+  //when you click treat, uhhhh
+}
+
+function increaseCount() {
+  z += 5;
+}
+
+function decreaseCount() {
+  z += -5;
+}
+
+function draw() {
+  image(halloween, 0, 0);
+
+push();
+imageMode(CENTER);
+  translate(350, 315);
+   rotate(radians(angle));
+  // rotate(angle);
+  image(skull, 0, 0);
+  angle += z;
+pop();
+
+  tint(255, 50);
+  var targetX = (mouseX-200)-x;
+  x += targetX*easing;
+
+  var targetY = (mouseY-100)-y;
+  y += targetY*easing;
+
+  image(ghosty, x, y);
+
+  textSize(15);
+  fill(255);
+  text('KEEP CLICKING', s, 410);
+  text('TO SPIN FASTER', s, 428);
+}
--- Week 7.5/homework.js
+++ Week 7.5/homework.js
@@ -1,67 +1,14 @@
-var halloween;
-var ghosty;
-var skull;
-var x = 0;
-var y = 0;
-var z = 0;
-var easing = 0.02;
-var angle = 0;
-var faster;
-var slower;
-var s = 290;
-
 function preload() {
-  halloween = loadImage('halloween.jpg');
-  ghosty = loadImage('ghosty.png');
-  skull = loadImage('skull.png');
+  pink = loadImage('pink.jpg');
+  fish = loadImage('fish.png');
 }
 
 function setup() {
-  createCanvas(800, 480);
-
-  faster = createButton('>');
-  faster.position(360, 370);
-  faster.mousePressed(increaseCount);
-
-  slower = createButton('<');
-  slower.position(315, 370);
-  slower.mousePressed(decreaseCount);
-
-  //when you click trick the skull will spin faster
-  //when you click treat, uhhhh
-}
-
-function increaseCount() {
-  z += 5;
-}
-
-function decreaseCount() {
-  z += -5;
+  createCanvas(800, 600);
 }
 
 function draw() {
-  image(halloween, 0, 0);
 
-push();
-imageMode(CENTER);
-  translate(350, 315);
-   rotate(radians(angle));
-  // rotate(angle);
-  image(skull, 0, 0);
-  angle += z;
-pop();
-
-  tint(255, 50);
-  var targetX = (mouseX-200)-x;
-  x += targetX*easing;
-
-  var targetY = (mouseY-100)-y;
-  y += targetY*easing;
-
-  image(ghosty, x, y);
-
-  textSize(15);
-  fill(255);
-  text('KEEP CLICKING', s, 410);
-  text('TO SPIN FASTER', s, 428);
+  image(pink, 0, 0);
+  image(fish, mouseX-120, mouseY-120);
 }
